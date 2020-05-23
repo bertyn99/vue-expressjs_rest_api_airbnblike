8b986e30f461b88ee545a09b6186665e99d7d85f
bertyn <bertynboulikou@gmail.com>
2020-05-23 16:37:50 +0200

    debut du routage

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Accueil");
+});
+
+app.get("/api/location", (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+app.get("/api/location/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
