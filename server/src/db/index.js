fc433c2f0ce7511d135ae52971144b790c50c6de
bertyn99 <bboulikou@gmail.com>
2020-06-03 16:58:50 +0200

    switch ot maria db

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,1 +1,23 @@
-const mysql = require('mysql')
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+     host: 'localhost', 
+     user:'admin', 
+     password: '',
+     connectionLimit: 10
+});
+async function asyncFunction() {
+    let conn;
+    try {
+      conn = await pool.getConnection();
+      const rows = await conn.query("SELECT 1 as val");
+      console.log(rows); //[ {val: 1}, meta: ... ]
+      const res = await conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+      console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+  
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) return conn.end();
+    }
+  }
