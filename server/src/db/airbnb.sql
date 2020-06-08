715699accc31e7c4864fc86ec100a973418b7c6b
bertyn99 <bboulikou@gmail.com>
2020-06-08 19:47:07 +0200

    update model db

--- server/src/db/airbnb.sql
+++ server/src/db/airbnb.sql
@@ -1,16 +1,16 @@
 
 CREATE TABLE IF NOT EXISTS users (
-    iduser INT NOT NULL PRIMARY KEY,
+    iduser INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255),
     prenom VARCHAR(255),
     tel INT,
-    email INT,
-    pasword  VARCHAR(50),
+    email VARCHAR(255),
+    password  VARCHAR(50),
     host BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS goods (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idhost INT,
     name VARCHAR(50),
     nbrChambre INT,
@@ -19,14 +19,14 @@ CREATE TABLE IF NOT EXISTS goods (
 );
 
 CREATE TABLE IF NOT EXISTS bed(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idgood INT,
     bedtyp ENUM('2 place','1 place'),
     nbrbed INT,
     CONSTRAINT fk_bed_goods_id FOREIGN KEY (idgood) REFERENCES goods(id)
 );
 CREATE TABLE IF NOT EXISTS facilities(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idgood INT,
     gym BOOLEAN,
     pool BOOLEAN,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS facilities(
 );
 
 CREATE TABLE IF NOT EXISTS Amenities(
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idgood INT,
     kitchen BOOLEAN,
     shampoo BOOLEAN,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS Amenities(
 );
 
 CREATE TABLE IF NOT EXISTS reservation (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     idclient INT,
     idgood INT NOT NULL,
     debutsejour DATE ,
