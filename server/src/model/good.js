5d56fe499719ff19917b575c3c692a623f74b5b8
bertyn99 <bboulikou@gmail.com>
2020-06-12 11:57:54 +0200

    back add model entry from

---
+++ server/src/model/good.js
@@ -0,0 +1,14 @@
+
+
+class Good{
+
+    constructor(idhost,name,nbrPlace,descripton,pricePp,addresse){
+        this.idhost = idhost;
+        this.name=name;
+        this.nbrPlace=nbrPlace;
+        this.descripton=descripton;
+        this.priceRp=pricePp;
+        this.addresse = addresse
+    }
+
+}
---
+++ server/src/model/localisation.js
@@ -0,0 +1,11 @@
+module.exports= class Localisation {
+    constructor(city, streetaddress, road, code, details){
+        this.city=city;
+        this.streetaddress =streetaddress;
+        this.road=road;
+        this.code= code;
+        this.details=details;
+    }
+}
+
+
