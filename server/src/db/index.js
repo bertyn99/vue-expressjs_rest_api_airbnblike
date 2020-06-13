6a9b24cabd388b742f33607b63c4d09ff0550f44
bertyn <bertynboulikou@gmail.com>
2020-06-13 20:00:50 +0200

    bdd add getRealEstate for a user

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,97 +1,49 @@
 const mysql = require('mysql');
 
-const pool= mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit: 5,
-    host: 'localhost', 
-     database:'airbnb',
-     user:'root', 
-     password: ''
- 
+    host: 'localhost',
+    database: 'airbnb',
+    user: 'root',
+    password: ''
+
 });
 
-let db={};
-db.allUser= ()=>{
-    return new Promise((resolve,reject) =>{
-        pool.query('SELECT * from users', (err, result)=>{
-            if(err){
+let db = {};
+db.allUser = () => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * from users', (err, result) => {
+            if (err) {
                 return reject(err);
             }
             return resolve(result)
         });
     });
 };
-db.newUser= (user)=>{
-    return new Promise((resolve,reject) =>{
-        pool.query('INSERT INTO users (nom,prenom,tel,email,password,host) VALUES (?,?,?,?,?,?)',[user.nom, user.prenom, user.tel, user.email,user.password,user.host], (err, result)=>{
-            if(err){
+db.newUser = (user) => {
+    return new Promise((resolve, reject) => {
+        pool.query('INSERT INTO users (nom,prenom,tel,email,password,host) VALUES (?,?,?,?,?,?)', [user.nom, user.prenom, user.tel, user.email, user.password, user.host], (err, result) => {
+            if (err) {
                 return reject(err);
             }
             return resolve(result)
         });
     });
 };
-db.getUser= (id)=>{
-    return new Promise((resolve,reject) =>{
-        pool.query('SELECT * from users WHERE iduser = ?',[id], (err, result)=>{
-            if(err){
+db.getUser = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * from users WHERE iduser = ?', [id], (err, result) => {
+            if (err) {
                 return reject(err);
             }
             return resolve(result)
         });
     });
 };
-db.updateUser= (id)=>{
-    return new Promise((resolve,reject) =>{
-        pool.query('SELECT * from users WHERE iduser = ?',[id], (err, result)=>{
-            if(err){
-                return reject(err);
-            }
-            return resolve(result)
-        });
-    });
-};
-
-db.newRealEstate= (good)=>{
-    return new Promise((resolve,reject) =>{
-        const sel= `SELECT id FROM realestate WHERE name=\'${good.name}\' AND idhost=${good.idhost} AND pricePp=${good.pricePp}`
-        pool.query('INSERT INTO realEstate (idhost,name,nbrPlace,description,pricePp) VALUES (?,?,?,?,?,?);\n'+
-                'INSERT INTO location (city,streetaddress,road,code,details) VALUES (?,?,?,?,?)',[good.idhost,good.name,good.nbrPlace,good.description,goodpriceRp,sel, good.streetaddress, good.road, good.code, good.details], (err, result)=>{
-           
-                      if(err){
-                return reject(err);
-            }
-            return resolve(result)
-        });
-    });
-};
-db.getRealEstate= ()=>{
-    return new Promise((resolve,reject) =>{
-        pool.query('SELECT * from users WHERE iduser = ?',[id], (err, result)=>{
-            if(err){
-                return reject(err);
-            }
-            return resolve(result)
-        });
-    });
-};
-db.updateRealEstate= (good)=>{
-    return new Promise((resolve,reject) =>{
-       /*  const sel= `SELECT id FROM realestate WHERE name=\'${good.name}\' AND idhost=${good.idhost} AND pricePp=${good.pricePp}`
-   */      pool.query('INSERT INTO realEstate (idhost,name,nbrPlace,description,pricePp) VALUES (?,?,?,?,?,?);\n'+
-                'INSERT INTO location (city,streetaddress,road,code,details) VALUES (?,?,?,?,?)',[good.idhost,good.name,good.nbrPlace,good.description,goodpriceRp,sel, good.streetaddress, good.road, good.code, good.details], (err, result)=>{
-           
-                    
-                    if(err){
-                return reject(err);
-            }
-            return resolve(result)
-        });
-    });
-};
-db.getAllRealEstate= ()=>{
-    return new Promise((resolve,reject) =>{
-        pool.query('SELECT * from realEstate', (err, result)=>{
-            if(err){
+db.updateUser = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * from users WHERE iduser = ?', [id], (err, result) => {
+            if (err) {
                 return reject(err);
             }
             return resolve(result)
@@ -99,5 +51,64 @@ db.getAllRealEstate= ()=>{
     });
 };
 
+db.newRealEstate = (good) => {
+    return new Promise((resolve, reject) => {
+        const sel = `SELECT id FROM realestate WHERE name=\'${good.name}\' AND idhost=${good.idhost} AND pricePp=${good.pricePp}`
+        pool.query('INSERT INTO realEstate (idhost,name,nbrPlace,description,pricePp) VALUES (?,?,?,?,?,?);\n' +
+            'INSERT INTO location (city,streetaddress,road,code,details) VALUES (?,?,?,?,?)', [good.idhost, good.name, good.nbrPlace, good.description, goodpriceRp, sel, good.streetaddress, good.road, good.code, good.details], (err, result) => {
 
-module.exports=db;
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result)
+            });
+    });
+};
+db.getRealEstate = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * from realEstate WHERE id = ?', [id], (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result)
+        });
+    });
+};
+db.getRealEstateOfUser = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * from realEstate WHERE idhost = ?', [id], (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result)
+        });
+    });
+};
+db.updateRealEstate = (good) => {
+    return new Promise((resolve, reject) => {
+        /*  const sel= `SELECT id FROM realestate WHERE name=\'${good.name}\' AND idhost=${good.idhost} AND pricePp=${good.pricePp}`
+         */
+        pool.query('INSERT INTO realEstate (idhost,name,nbrPlace,description,pricePp) VALUES (?,?,?,?,?,?);\n' +
+            'INSERT INTO location (city,streetaddress,road,code,details) VALUES (?,?,?,?,?)', [good.idhost, good.name, good.nbrPlace, good.description, goodpriceRp, sel, good.streetaddress, good.road, good.code, good.details], (err, result) => {
+
+
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result)
+            });
+    });
+};
+db.getAllRealEstate = () => {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT * from realEstate', (err, result) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(result)
+        });
+    });
+};
+
+
+module.exports = db;
