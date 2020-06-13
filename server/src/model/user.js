bc667303c228fb8308784cb7cde872024b5794f8
bertyn <bertynboulikou@gmail.com>
2020-06-13 20:01:26 +0200

    model add user

---
+++ server/src/model/user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(firstname, lastname, tel, email, passwd, host) {
+
+        this.nom = firstname;
+        this.prenom = lastname;
+        this.tel = tel;
+        this.email = email;
+        this.passwd = passwd;
+        this.host = false
+    }
+}
