a45c7f9dedab503a4be7dbdf2fae4bf31ac35624
bertyn99 <bboulikou@gmail.com>
2020-06-17 09:40:47 +0200

    client add prop to component Annoncecard

---
+++ client/src/components/AnnonceCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="annonce-card">
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AnnonceCard',
+  props: {
+    nom: String,
+    description: String,
+    Place: String,
+    pricePp: Number
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
