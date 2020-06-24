b92172f6732ac9bff3b48bebf63e4ab913d8007f
bertyn99 <bboulikou@gmail.com>
2020-06-24 09:33:06 +0200

    fix typo

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -68,28 +68,28 @@ export default new Vuex.Store({
     loginUser ({ commit }, user) {
       commit('SET_CURRENT_USER', user)
     },
-    async login({commit, dispatch}, loginInfo) {
+    async login ({ commit }, loginInfo) {
       try {
-        let response = await Api().post('/sessions', loginInfo);
-        let user = response.data.data.attributes;
+        const response = await Api().post('/sessions', loginInfo)
+        const user = response.data.data.attributes
 
-        commit('SET_CURRENT_USER', user);
-        return user;
+        commit('SET_CURRENT_USER', user)
+        return user
       } catch {
-        return {error: "Email/password combination was incorrect.  Please try again."}
-      }   
-    },
-    async register({commit, dispatch}, registrationInfo) {
-      try {
-        let response = await Api().post('/users', registrationInfo);
-        let user = response.data.data.attributes;
-
-        commit('SET_CURRENT_USER', user);
-        return user;
-      } catch {
-        return { error: "There was an error.  Please try again." }
+        return { error: 'Email/password combination was incorrect.  Please try again.' }
       }
     },
+    async register ({ commit }, registrationInfo) {
+      try {
+        const response = await Api().post('/users', registrationInfo)
+        const user = response.data.data.attributes
+
+        commit('SET_CURRENT_USER', user)
+        return user
+      } catch {
+        return { error: 'There was an error.  Please try again.' }
+      }
+    }
   },
   modules: {
   }
