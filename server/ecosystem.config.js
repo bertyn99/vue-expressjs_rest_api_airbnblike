2edf754d519978e26ef87b30e703eb8adf1c65fd
bertyn99 <bboulikou@gmail.com>
2020-08-01 14:51:46 +0200

    move eco

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     script: 'server/index.js',
     watch: '.',
-    env: {
+    env_production: {
       "NODE_ENV": "producion",
       "DB_USER": "root",
       "DB_PASSWORD": "persival99",
@@ -18,7 +18,10 @@ module.exports = {
       repo : 'git@github.com:bertyn99/vue-expressjs_rest_api_airbnblike.git',
       path : '/home/bertyn/project/serveur_api',
       'pre-deploy-local': '',
-      'post-deploy' : 'cd server | npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'cd server | npm i && pm2 reload ecosystem.config.js --env production',
+      "env"  : {
+        "NODE_ENV": "production"
+      },
       'pre-setup': ''
     }
   }
