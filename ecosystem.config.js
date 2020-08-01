bc1079c57fde11a7513e18efd572b398edc30190
bertyn99 <bboulikou@gmail.com>
2020-08-01 14:35:15 +0200

    fix dossierr

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,24 +3,22 @@ module.exports = {
     script: 'server/index.js',
     watch: '.',
     env: {
+      "NODE_ENV": "producion",
       "DB_USER": "root",
       "DB_PASSWORD": "persival99",
       "TOKEN_SECRET":  "azuouscoughsbqcgbuehbeiussbffg"
     }
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
   }],
 
   deploy : {
     production : {
       user : 'bertyn',
-      host : '51.91.8.181',
+      host : ['51.91.8.181'],
       ref  : 'origin/master',
-      repo : 'https://github.com/bertyn99/vue-expressjs_rest_api_airbnblike',
+      repo : 'git@github.com:bertyn99/vue-expressjs_rest_api_airbnblike.git',
       path : '/home/bertyn/project/serveur_api',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'cd server | npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
